1dea2aae0f6eda232c462b4a13276c9d53041601
kotano <kot4no@gmail.com>
2020-11-28 03:57:37 +0300

    Add scripts and links download

--- page_loader/loader.py
+++ page_loader/loader.py
@@ -1,5 +1,6 @@
 from os.path import splitext
 from pathlib import Path
+import re
 from urllib.parse import urlparse
 
 import requests
@@ -12,7 +13,10 @@ def download(url, destination=Path.cwd()) -> str:
     dir_path.mkdir(exist_ok=True)
 
     contents = requests.get(url).text
-    contents = load_images(contents, dir_path)
+    contents = load_resources(
+        contents, dir_path,
+        # match=urlparse(url).hostname
+    )
 
     page_path = Path(destination).resolve() / (file_name + '.html')
     page_path.write_text(contents, 'utf-8')
@@ -27,17 +31,23 @@ def get_filename_by_url(url) -> str:
     return file_name
 
 
-def load_images(contents, destination) -> str:
+tag_map = {
+    'img': 'data-src',
+    'script': 'src',
+    'link': 'href',
+}
+
+
+def load_resources(contents, destination, match='') -> str:
     soup = BeautifulSoup(contents, features='html.parser')
-    for tag in soup.find_all('img', recursive=True):
-        src = tag['data-src']
-        base, ext = splitext(src)
-        file_path = Path(destination) / (get_filename_by_url(base) + ext)
-        response = requests.get(src)
-        file_path.write_bytes(response.content)
-        tag['data-src'] = (Path(destination.name) / file_path.name).as_posix()
+    for tag, attr in tag_map.items():
+        for elem in soup.find_all(tag, recursive=True):
+            src = elem.get(attr)
+            if not src or not re.search(match, src):
+                continue
+            base, ext = splitext(src)
+            file_path = Path(destination) / (get_filename_by_url(base) + ext)
+            response = requests.get(src)
+            file_path.write_bytes(response.content)
+            elem[attr] = (Path(destination.name) / file_path.name).as_posix()
     return str(soup)
-
-
-def load_local_files():
-    pass
